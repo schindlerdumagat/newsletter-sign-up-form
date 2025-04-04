36effb2fda56e417e5b79ac3cee74bd4b3b6a6b6
schindlerwork <schindler@xtendly.com>
2025-04-04 15:59:21 +0800

    feat: Completed Project v.1

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form");
+const emailInput = document.getElementById("email");
+const inputError = document.getElementById("error");
+const formPage = document.getElementById("form-page");
+const successPage = document.getElementById("success-page");
+const successBtn = document.getElementById("success-btn");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const emailValue = Object.fromEntries(formData.entries()).email;
+
+    if (emailValue.trim() === "") {
+        emailInput.classList.add("card__input--error");
+        inputError.innerText = "Email is required";
+        return;
+    } else if (!emailValue.match(/^\S+@\S+$/g)) {
+        emailInput.classList.add("card__input--error");
+        inputError.innerText = "Valid email required";
+        return;
+    }
+
+    successPage.classList.remove("hidden");
+    formPage.classList.add("hidden");
+
+})
+
+emailInput.addEventListener("focus", () => {
+    if (emailInput.classList.contains("card__input--error")) {
+        emailInput.classList.remove("card__input--error");
+        inputError.innerText = "";
+    }
+});
+
+successBtn.addEventListener("click", () => {
+    successPage.classList.add("hidden");
+    formPage.classList.remove("hidden");
+    emailInput.value = "";
+})
