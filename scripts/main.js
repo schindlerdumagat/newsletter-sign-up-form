9d5115f45ffb17e4b619ae817b6eab824576be06
schindlerwork <schindler@xtendly.com>
2025-04-07 16:42:01 +0800

    refactor: form handling logic

--- scripts/main.js
+++ scripts/main.js
@@ -9,31 +9,29 @@ const successEmail = document.getElementById("success-email");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    const formData = new FormData(e.target);
-    const emailValue = Object.fromEntries(formData.entries()).email.toLocaleLowerCase().trim();
-
-    if (emailValue === "") {
-        emailInput.classList.add("card__input--error");
-        inputError.innerText = "Email is required";
-        return;
-    } else if (!emailValue.match(/^\S+@\S+$/g)) {
-        emailInput.classList.add("card__input--error");
-        inputError.innerText = "Valid email required";
+    if (!emailInput.validity.valid) {
+        showError();
         return;
     }
 
-    successEmail.innerText = emailValue;
-    successPage.classList.remove("hidden");
+    successEmail.innerText = emailInput.value;
+    inputError.textContent = "";
     formPage.classList.add("hidden");
+    successPage.classList.remove("hidden");
+    emailInput.classList.remove("card__input--error");
 
-})
-
-emailInput.addEventListener("focus", () => {
-    if (emailInput.classList.contains("card__input--error")) {
-        emailInput.classList.remove("card__input--error");
-        inputError.innerText = "";
-    }
 });
+
+emailInput.addEventListener("input", () => {
+    
+    if (!emailInput.validity.valid) {
+        showError();
+        return;
+    }
+
+    emailInput.classList.remove("card__input--error");
+    inputError.textContent = "";
+})
 
 successBtn.addEventListener("click", () => {
     emailInput.value = "";
@@ -41,3 +39,15 @@ successBtn.addEventListener("click", () => {
     successPage.classList.add("hidden");
     formPage.classList.remove("hidden");
 })
+
+function showError() {
+
+    if (emailInput.validity.valueMissing) {
+        inputError.textContent = "Email is required";
+    } else  if (emailInput.validity.typeMismatch) {
+        inputError.textContent = "Valid email required"
+    }
+
+    emailInput.classList.add("card__input--error");
+
+}
