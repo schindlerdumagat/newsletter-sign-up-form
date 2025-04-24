27186966180dee660c648061cb75c9abbc45ac35
schindlerwork <schindler@xtendly.com>
2025-04-24 23:58:35 +0800

    add: aria-hidden, refactor script

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById("form");
+const formPage = document.getElementById("form-page");
 const emailInput = document.getElementById("email");
 const inputError = document.getElementById("error");
-const formPage = document.getElementById("form-page");
 const successPage = document.getElementById("success-page");
 const successBtn = document.getElementById("success-btn");
 const successEmail = document.getElementById("success-email");
@@ -9,45 +9,58 @@ const successEmail = document.getElementById("success-email");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    if (!emailInput.validity.valid) {
-        showError();
+    if(!isEmailValid()) {
         return;
     }
 
     successEmail.innerText = emailInput.value;
     inputError.textContent = "";
-    formPage.classList.add("hidden");
-    successPage.classList.remove("hidden");
     emailInput.classList.remove("card__input--error");
+
+    successPage.removeAttribute("aria-hidden");
+    successPage.classList.remove("hidden");
+    formPage.setAttribute("aria-hidden", true);
+    formPage.classList.add("hidden");
 
 });
 
 emailInput.addEventListener("input", () => {
     
-    if (!emailInput.validity.valid) {
-        showError();
+    if(!isEmailValid()) {
         return;
     }
 
-    emailInput.classList.remove("card__input--error");
     inputError.textContent = "";
-})
+    emailInput.classList.remove("card__input--error");
+});
 
 successBtn.addEventListener("click", () => {
     emailInput.value = "";
     successEmail.innerText = "";
+    
+    successPage.setAttribute("aria-hidden", true);
     successPage.classList.add("hidden");
+    formPage.removeAttribute("aria-hidden");
     formPage.classList.remove("hidden");
-})
+});
 
-function showError() {
+// Check if email is valid
+function isEmailValid () {
 
-    if (emailInput.validity.valueMissing) {
-        inputError.textContent = "Email is required";
-    } else  if (emailInput.validity.typeMismatch) {
-        inputError.textContent = "Valid email required"
+    let isValid = true;
+
+    if (!emailInput.validity.valid) {
+
+        // Set error message when email is invalid
+        if (emailInput.validity.valueMissing) {
+            inputError.textContent = "Email is required";
+        } else  if (emailInput.validity.typeMismatch) {
+            inputError.textContent = "Valid email required"
+        }
+
+        emailInput.classList.add("card__input--error");
+        isValid = false;
     }
 
-    emailInput.classList.add("card__input--error");
-
+    return isValid;
 }
